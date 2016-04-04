38be7a0d5423de8bed44733269922b377b18fb51
Jason <jason.arita@gmail.com>
2016-04-04 10:52:36 -0700

    Added in notes for code generalizability, reusability, modules, handling input args, python env enforcement
    Covered:
    
    - Generalizability & `main` function
    - Code reusability & modules
    - Handling input args via ARGPARSER
    - Enforcing a python version via sh-bang `#! /usr/bin/env python3.x`

---
+++ jfaUtils.py
@@ -0,0 +1,14 @@
+def load_sequences(filename):
+    list_of_seqs = []
+    
+    fp = open(filename)
+    for count, line in enumerate(fp):
+        if (count % 4) == 1:
+            seq = line.strip()
+            list_of_seqs.append(seq)
+    return list_of_seqs
+
+def count_Ns(seq):
+    n_bases = len(seq)
+    n_ns = seq.count('N')
+    return (n_bases, n_ns)
